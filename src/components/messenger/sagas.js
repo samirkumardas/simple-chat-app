7eea1f7175bfcc964d5bbdb6d5d1ae4d89c2321a
Samir <cse.samir@gmail.com>
2018-10-23 22:10:13 +0600

    ready to push

--- src/components/messenger/sagas.js
+++ src/components/messenger/sagas.js
@@ -3,25 +3,12 @@ import connector from '../../utils/connector';
 import { storage, getTypeFromKey } from '../../utils/helper';
 import CONSTANTS from '../../config/constants';
 
-import { setNotice } from '../notice/reducer'; //removeNotice
-//import { showLoader, hideLoader } from '../loader/reducer';
-//import { hideModal } from '../modal/reducer';
+import { setNotice } from '../notice/reducer';
 import { getMessages, setMessages, sendMessage, addMessage } from '../messenger/reducer';
 import { doLogout } from '../dashboard/reducer';
 
-/*
-function* doPreStuff() {
-    yield put(removeNotice());
-    yield put(showLoader());
-}
-
-function* doPostStuff() {
-    yield put(hideLoader());
-} */
-
 function* doErrorStuff(err) {
     let message = err.message || err.errorDesc;
-    //yield put(hideLoader());
     if (message == 'INVALID_SESSION') {
         message = 'Your session has been expired. Please login.';
         yield put(doLogout());
@@ -38,7 +25,6 @@ function* workerGetMessages(action) {
           key = action.payload.key,
           {type, id} = getTypeFromKey(key);
     try {
-        //yield call(doPreStuff);
         const data = {
             'act': 'messages',
             id,
@@ -50,7 +36,6 @@ function* workerGetMessages(action) {
             key,
             messages: response.messages
         }));
-        //yield call(doPostStuff);
     } catch (err) {
         yield call(doErrorStuff, err);
     }
@@ -60,7 +45,6 @@ export function* watcherGetMessages() {
     yield takeLatest(getMessages.getType(), workerGetMessages);
 }
 
-
 /* send message */
 function* workerSendMessage(action) {
     const token = storage.get(CONSTANTS.AUTH_KEY),
@@ -69,7 +53,6 @@ function* workerSendMessage(action) {
           {type, id} = getTypeFromKey(key);
 
     try {
-        //yield call(doPreStuff);
         const data = {
             'act': 'message',
             token,
@@ -81,7 +64,6 @@ function* workerSendMessage(action) {
         yield put(addMessage({
            message: response
         }));
-        //yield call(doPostStuff);
     } catch (err) {
         yield call(doErrorStuff, err);
     }
